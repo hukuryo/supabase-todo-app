b3d13ac75ed7f912eadf5e17db718564f7dff65b
hukuryo <ryohei.1998.05.09@gmail.com>
2024-02-18 11:40:36 +0900

    TODOの一覧を表示するコンポーネントを作成

---
+++ app/todos/page.tsx
@@ -0,0 +1,10 @@
+import TodoList from "@/components/TodoList"
+
+export default function (){
+    return(
+        <>
+            <h1>TODO LIST</h1>
+            <div><TodoList /></div>
+        </>
+    )
+}
