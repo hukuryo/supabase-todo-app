cc970dc98559d029c8dc329e4cc839032c887be4
hukuryo <ryohei.1998.05.09@gmail.com>
2024-02-19 22:03:59 +0900

    shadcnをインストール

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,8 +1,11 @@
 import TodoList from "@/components/TodoList"
+import Header from "@/components/Header"
+
 
 export default function (){
     return(
-        <>
+        <>  
+            <Header />
             <h1>TODO LIST</h1>
             <div><TodoList /></div>
         </>
