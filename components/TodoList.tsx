b40f5784a77f502c7f15685b4b2600a1ab75e67e
hukuryo <ryohei.1998.05.09@gmail.com>
2024-02-19 23:07:18 +0900

    リストで表示できるようにした

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,8 +1,52 @@
 import { createClient } from '@/utils/supabase/server';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+
+  type todos = {
+    id: number;
+    title: string;
+    contents: string;
+    start_date: Date;
+    end_date: Date;
+  }
+  
 
 export default async function TodoList() {
   const supabase = createClient();
-  const { data: todos } = await supabase.from("todos").select();
+  const { data: todos = [] } = await supabase.from("todos").select();
+  const todoList: todos[] = todos || [];
 
-  return <pre>{JSON.stringify(todos, null, 2)}</pre>
+  return (
+    <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+            <TableRow>
+            <TableHead className="w-[100px]">id</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>content</TableHead>
+            <TableHead className="text-right">start</TableHead>
+            <TableHead className="text-right">end</TableHead>
+            </TableRow>
+        </TableHeader>
+        
+        <TableBody>
+            {todoList && todoList.map((todo) => (
+                <TableRow key={todo.id}>
+                    <TableCell className="font-medium">{todo.id}</TableCell>
+                    <TableCell>{todo.title}</TableCell>
+                    <TableCell>{todo.contents}</TableCell>
+                    <TableCell>{todo.start_date.toString()}</TableCell>
+                    <TableCell className="text-right">{todo.end_date.toString()}</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+    </Table>
+);
 }
